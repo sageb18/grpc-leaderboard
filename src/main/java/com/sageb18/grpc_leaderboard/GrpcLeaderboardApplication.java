97917251499832d0ca8ff9cf690b5cd2637a3c01
sageb18 <taddeosage@gmail.com>
2026-08-26 21:45:03 -0700

    Initial commit

---
+++ src/main/java/com/sageb18/grpc_leaderboard/GrpcLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.sageb18.grpc_leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcLeaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sageb18/grpc_leaderboard/GrpcLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sageb18.grpc_leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
